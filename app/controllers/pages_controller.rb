f89935c0c63dfb628d5222c5f029383c91d45bbe
Brendon <bwells@whisk.co.uk>
2013-08-06 17:45:00 +0100

    button fix + fix to take back live

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,9 @@ class PagesController < ApplicationController
   def home
   end
 
-<<<<<<< HEAD
   def  action
-=======
+
   def action 
->>>>>>> e66793ce422c2e4a0c086b9b7029aaf178a33d28
   end
  end
 
